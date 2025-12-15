c6d266cb11c5d27e699fe5367e3a495aed08ccdb
Aurelio Maica <aureliomaica@gmail.com>
2025-12-15 10:45:45 -0300

    otimizaçoes_geraris

--- src/openalpr/alpr_impl.cpp
+++ src/openalpr/alpr_impl.cpp
@@ -1,21 +1,4 @@
-/*
- * Copyright (c) 2015 OpenALPR Technology, Inc.
- * Open source Automated License Plate Recognition [http://www.openalpr.com]
- *
- * This file is part of OpenALPR.
- *
- * OpenALPR is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License
- * version 3 as published by the Free Software Foundation
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+
 
 #include "alpr_impl.h"
 #include "result_aggregator.h"
@@ -245,6 +228,8 @@ namespace alpr
     // Prewarp the image and ROIs if configured]
     std::vector<cv::Rect> warpedRegionsOfInterest = effectiveRois;
     // Warp the image if prewarp is provided
+    detectColor = prewarp->warpImage(detectColor);
+    procColor = prewarp->warpImage(procColor);
     detectGray = prewarp->warpImage(detectGray);
     procGray = prewarp->warpImage(procGray);
     warpedRegionsOfInterest = prewarp->projectRects(effectiveRois, detectGray.cols, detectGray.rows, false);
